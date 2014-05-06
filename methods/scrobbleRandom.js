25585e22a3237c5ecb22ce0cfa03611019869199
Georgy Chirkov <mail@gich.co>
2014-05-06 16:28:20 +0400

    added scrobbleRandom method

--- methods/scrobbleRandom.js
+++ methods/scrobbleRandom.js
@@ -1,3 +1,25 @@
-/**
- * Created by adm on 06.05.2014.
- */
+var async = require('async');
+
+module.exports = function (params,lfm ) {
+    async.forever(
+        function (next) {
+            var artist = Math.random().toString(36).substring(7)
+                , track = Math.random().toString(36).substring(7);
+            lfm.track.scrobble({
+                'artist': artist,
+                'track': track,
+                'timestamp': Math.floor((new Date()).getTime() / 1000) + Math.floor(Math.random() * 1000) - 500
+            }, function (err, scrobbles) {
+                params.log && console.log('We have just scrobbled:', scrobbles);
+                setTimeout(function () {
+                    next(err, scrobbles)
+                }, 450 + Math.floor(Math.random() * 100) + (Math.random() > .9 ? 1500 : 0));
+            });
+        },
+        function (err) {
+            if (err) {
+                return console.log('We\'re in trouble', err);
+            }
+        }
+    );
+};
